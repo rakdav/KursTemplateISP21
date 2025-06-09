0bb4fabf5c14252db7dc423291cd62802481c78c
rakdav <rakdav@inbox.ru>
2025-06-09 15:34:41 +0200

    Добавьте файлы проекта.

---
+++ KursProjectISP31/Utills/RelayCommandSQL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KursProjectISP31.Utills
+{
+    public class RelayCommandSQL:ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        private Action DoWork;
+        public RelayCommandSQL(Action work)
+        {
+            DoWork = work;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+        public void Execute(object parameter)
+        {
+            DoWork();
+        }
+    }
+}
---
+++ KursProjectISP31/ViewModel/NavigationViewModel.cs
@@ -0,0 +1,30 @@
+﻿using KursProjectISP31.Utills;
+using KursProjectISP31.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace KursProjectISP31.ViewModel
+{
+    public class NavigationViewModel:ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+        public ICommand HomeCommand { get; set; }
+
+        private void Home(object obj) => CurrentView = new HomeViewModel();
+        public NavigationViewModel()
+        {
+            HomeCommand = new RelayCommand(Home);
+            // Startup Page
+            CurrentView = new HomeViewModel();
+        }
+    }
+}
